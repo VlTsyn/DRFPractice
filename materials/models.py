d45b5c382f3d81bd55b3d4ae2909ef38113703b2
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2026-05-03 15:48:05 +0300

    add views and serializers, add urls

--- materials/models.py
+++ materials/models.py
@@ -7,6 +7,9 @@ class Course(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
     image = models.ImageField(upload_to="courses/images", blank=True, null=True, verbose_name="Превью курса")
 
+    def __str__(self):
+        return self.name
+
 
     class Meta:
         verbose_name = "Курс"
@@ -15,6 +18,7 @@ class Course(models.Model):
 
 class Lesson(models.Model):
 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name="Курс")
     name = models.CharField(max_length=100, verbose_name="Название урока")
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
     image = models.ImageField(upload_to="lessons/images", blank=True, null=True, verbose_name="Превью урока")
