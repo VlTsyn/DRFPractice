bd077ce2d63005823e1140d407803e8ea3d9814d
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2026-05-14 04:11:35 +0300

    add info about lessons in course

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,22 +4,23 @@ from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    """Сериализация урока"""
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(ModelSerializer):
     """Сериализация курса"""
 
     lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lessons_count(self, course):
         return course.lessons.count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(ModelSerializer):
-    """Сериализация урока"""
-
-    class Meta:
-        model = Lesson
         fields = '__all__'
